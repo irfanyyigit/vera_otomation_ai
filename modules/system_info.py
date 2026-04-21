d9e147c4488826d233a4b478fa2d7c5025bfea4b
İrfan Yiğit <irfanyigit341@gmail.com>
2026-04-21 19:10:09 +0300

    update system_info.py

--- modules/system_info.py
+++ modules/system_info.py
@@ -7,7 +7,7 @@ import subprocess
 from streamlit_autorefresh import st_autorefresh
 
 # 1. Otomatik Yenileme (Sayfa her 2 saniyede bir kendi kendine yenilenir)
-st_autorefresh(interval=2000, key="datarefresh")
+st_autorefresh(interval=2000, key="globalrefresh")
 
 def get_processor_name():
     # Windows'ta normal çalışsın, Linux'ta sistem dosyasından okusun
